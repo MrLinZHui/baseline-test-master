e8385621f5b9d5b3d51515c3f4ae4e85fb3e2b13
MrLinZHui <742847492@qq.com>
2019-07-08 12:05:48 +0800

    test3

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var array = [];
+  var array1 = collectionB.value;
+  console.log("array1："+array1);
+  for(var str= 0;str<collectionA.length;str++){
+    for(var b = 0;b<array1.length;b++){
+      if(collectionA[str]==array1[b]){
+        array.push(collectionA[str]);
+      }
+    }
+  }
+  return array;
 }
