4174bab9c6f24d8cddce73a248e073ecf4f562ce
MrLinZHui <742847492@qq.com>
2019-07-08 12:32:04 +0800

    test 3

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,9 +1,9 @@
 'use strict';
 
-function collectSameElements(collectionA, objectB) {
+function collectSameElements(collectionA, collectionB) {
   var array = [];
   var array1 = collectionB.value;
-  console.log("array1："+array1);
+  //console.log("array1："+array1);
   for(var str= 0;str<collectionA.length;str++){
     for(var b = 0;b<array1.length;b++){
       if(collectionA[str]==array1[b]){
